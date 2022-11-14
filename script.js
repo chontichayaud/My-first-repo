1e16a491df8336d4b6f929e191d9a5b0522eb27f
chontichayaud <chonticha270335@gmail.com>
2022-11-14 22:16:28 +0000

    p tag for inner html

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function displayDate(){
   n =  new Date();
   y = n.getFullYear();
   m = n.getMonth() + 1;
   d = n.getDate();
-  document.getElementById("date").innerHTML = d;}
+  document.getElementById("date").innerHTML = <p>${d}</p>;
 
