1d470b8afb18163c1cf976f5b6ab09f0222847f3
chontichayaud <chonticha270335@gmail.com>
2022-11-14 21:51:33 +0000

    just D

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ n =  new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
-document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+document.getElementById("date").innerHTML = d;
