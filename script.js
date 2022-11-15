c363ce997b530902f80b735468ff8fe3a2e42fd7
chontichayaud <chonticha270335@gmail.com>
2022-11-15 08:36:38 +0000

    start date recommendation

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-  let n =  new Date();
-  let y = n.getFullYear();
-  let m = n.getMonth() + 1;
-  let d = n.getDate();
+  let now =  new Date();
+  let y = now.getFullYear();
+  let m = now.getMonth();
+  let d = now.getDate();
+  let plus6Weeks = new Date() + 42;
 
-  document.querySelector("#date").append("The date is" + n.getDate());
+  document.querySelector("#date").append(d + "/" + m + "/" + y);
+  document.querySelector("#dateIn6Weeks").append(plus6Weeks.getDate() + "/" + plus6Weeks.getMonth() + "/" + plus6Weeks.getFullYear());
+
+  console.log(plus6Weeks);
   console.log("This worked");
