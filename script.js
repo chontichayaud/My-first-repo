14c171485a0cef60396b082c197591a510d05f77
chontichayaud <chonticha270335@gmail.com>
2022-11-15 08:42:25 +0000

    setDate() and 0 index for getMonth()

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
   let now =  new Date();
   let y = now.getFullYear();
-  let m = now.getMonth();
+  let m = now.getMonth() + 1;
   let d = now.getDate();
-  let plus6Weeks = new Date() + 42;
+  let plus6Weeks = new Date().setDate(now.getDate() + 42);
 
   document.querySelector("#date").append(d + "/" + m + "/" + y);
-  document.querySelector("#dateIn6Weeks").append(plus6Weeks.getDate() + "/" + plus6Weeks.getMonth() + "/" + plus6Weeks.getFullYear());
+  document.querySelector("#dateIn6Weeks").append(plus6Weeks.getDate() + "/" + (plus6Weeks.getMonth()+1) + "/" + plus6Weeks.getFullYear());
 
   console.log(plus6Weeks);
   console.log("This worked");
