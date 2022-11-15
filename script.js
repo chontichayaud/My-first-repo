019f02fc6f1df4e77cc05ac1366c5526e724afa2
chontichayaud <chonticha270335@gmail.com>
2022-11-15 09:03:18 +0000

    Moved console log in an effort to force dfeployment to actually work - js on deployment not updating despite being form correct commit which shows updated javascript

--- script.js
+++ script.js
@@ -4,9 +4,10 @@
   let m = now.getMonth() + 1;
   let d = now.getDate();
   plus6Weeks.setDate(plus6Weeks.getDate() + 42);
+  console.log(plus6Weeks);
+
 
   document.querySelector("#date").append(d + "/" + m + "/" + y);
   document.querySelector("#dateIn6Weeks").append(plus6Weeks.getDate() + "/" + (plus6Weeks.getMonth()+1) + "/" + plus6Weeks.getFullYear());
 
-  console.log(plus6Weeks);
   console.log("This worked");
