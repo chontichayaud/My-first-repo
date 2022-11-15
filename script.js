d82c695274e11cb4784c50499238a182c8c8d17c
chontichayaud <chonticha270335@gmail.com>
2022-11-15 08:12:39 +0000

    added let to variable in js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-  n =  new Date();
-  y = n.getFullYear();
-  m = n.getMonth() + 1;
-  d = n.getDate();
-  document.createElement("p");
-  p.textContent = n.getDate();
-  
-  document.querySelector("#date").append("The date is" + p);
+  let n =  new Date();
+  let y = n.getFullYear();
+  let m = n.getMonth() + 1;
+  let d = n.getDate();
+
+  document.querySelector("#date").append("The date is" + n.getDate());
   console.log("This worked");
